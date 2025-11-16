0d5af577a1925e59dc36ae71014e5d593d0bf52d
Preston Schlagheck <your.email@example.com>
2025-11-15 21:37:39 -0500

    Make hero image significantly smaller - 85% width
    - Changed from max-w-6xl to w-[85%] max-w-5xl
    - Image now 85% of container width
    - Max width: 1024px (was 1280px originally)
    - Much more noticeable size reduction
    - Still centered with mx-auto

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,13 +9,13 @@ export default function Hero() {
       {/* Animated Grid Background */}
       <div className="absolute inset-0 grid-background opacity-30" />
 
-      {/* Hero Image - Centered and 15% Smaller */}
+      {/* Hero Image - Centered and Smaller */}
       <div className="relative z-10 container-padding w-full">
         <motion.div
           initial={{ opacity: 0, scale: 0.95 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
-          className="max-w-6xl mx-auto"
+          className="w-[85%] max-w-5xl mx-auto"
         >
           <div className="relative w-full aspect-[16/9]">
             <Image
